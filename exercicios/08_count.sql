0a056e4138887d6fdedd26a9c40d9480b7ed0e3f
Cristiano Cintra <95584929+Crcintra@users.noreply.github.com>
2026-06-19 15:12:32 -0300

    08_COUNT.SQL

--- exercicios/08_count.sql
+++ exercicios/08_count.sql
@@ -1,9 +1,15 @@
--- SELECT count(*),
--- count(1),
--- count(distinct IdCliente)
+-- Exemplos de contagem
+
+-- Total de registros
+-- SELECT COUNT(*)
 -- FROM clientes;
 
+-- Outra forma de contar registros
+-- SELECT COUNT(1)
+-- FROM clientes;
 
-
-SELECT COUNT(DISTINCT IdCliente) AS TotalClientes
-FROM clientes;
+-- Total de clientes distintos
+SELECT
+    COUNT(DISTINCT IdCliente) AS TotalClientes
+FROM
+    clientes;
