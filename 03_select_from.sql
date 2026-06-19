be6aaaf86e5cfe77542b0786a5508c2a1f9d2949
Cristiano Cintra <95584929+Crcintra@users.noreply.github.com>
2026-06-19 14:38:51 -0300

    03_SELECT_FROM
    Added comments to clarify the purpose of the query.

--- 03_select_from.sql
+++ 03_select_from.sql
@@ -1,1 +1,9 @@
-SELECT * FROM transacoes;
+-- ==========================================
+-- CONSULTA TODOS OS REGISTROS DA TABELA
+-- ==========================================
+
+-- Exibe todas as colunas e todos os registros
+-- da tabela 'transacoes'.
+
+SELECT *
+FROM transacoes;
